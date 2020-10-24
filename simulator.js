a868e7a27efff77f70e830191e140ca4c16cba9f
Maarslet <12084712+Maarslet@users.noreply.github.com>
2020-10-24 23:56:31 +0200

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -173,7 +173,8 @@ function runSim() {
   var critVal = (critDPS-baseDPS);
   var hitVal  = (hitDPS-baseDPS);
   
-  var output = "Average dps from fight duration span of " + timeVec[0] + " to " + timeVec[timeVec.length-1] + " seconds is " + formatNumber(math.sum(DPS)/DPS.length,2) + " dps<br>";
+  //var output = "Average dps from fight duration span of " + timeVec[0] + " to " + timeVec[timeVec.length-1] + " seconds is " + formatNumber(math.sum(DPS)/DPS.length,2) + " dps<br>";
+  var output = "Average dps: " + formatNumber(math.sum(DPS)/DPS.length,2) + " dps<br>Fight durations: " + timeVec[0] + " to " + timeVec[timeVec.length-1] + " seconds<br><br>";
   output += "Stat Weights:<br>Crit = " + formatNumber(critVal/SPVal,2) + " SP<br>Hit &nbsp= " + formatNumber(hitVal/SPVal,2) + " SP";
   
   document.getElementById("page").innerHTML = output;
